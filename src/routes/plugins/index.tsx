26e9065b73e9f8aea543453ffe4a0b48b84f056a
nyagami <hoangquan05112002@gmail.com>
2024-10-18 10:31:23 +0700

    fix: allow all devices to add plugin repo

--- src/routes/plugins/index.tsx
+++ src/routes/plugins/index.tsx
@@ -1,14 +1,13 @@
 import Layout from "@components/Layout";
 import Page from "@components/Page";
 import { useTheme } from "@hooks/useTheme";
-import { Box, Button, Card, Divider, Typography } from "@mui/material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 
 const lnreaderPluginsRepo =
   "https://raw.githubusercontent.com/LNReader/lnreader-plugins/plugins/v3.0.0/.dist/plugins.min.json";
 
 export default function Plugins() {
   const theme = useTheme();
-  const isAndroid = /android/i.test(navigator.userAgent);
 
   return (
     <Layout>
@@ -24,46 +23,26 @@ export default function Plugins() {
               <b>LNReader</b> maintains only one official repository; any other
               repositories are unofficial and have no affiliation with us.
             </Typography>
-            {isAndroid ? (
-              <Box sx={{ my: 2, textAlign: "center" }}>
-                <Button
-                  variant="contained"
-                  sx={{
-                    borderRadius: 12,
-                    background: theme.primaryContainer,
-                    color: theme.onPrimaryContainer,
-                    textTransform: "none",
-                  }}
-                  href={
-                    "lnreader://repo/add?url=" +
-                    encodeURIComponent(lnreaderPluginsRepo)
-                  }
-                >
-                  Add repository
-                </Button>
-                <Typography sx={{ mt: 2 }}>
-                  Requires <b>LNReader 2.0.0</b> or newer.
-                </Typography>
-              </Box>
-            ) : (
-              <Card
+            <Box sx={{ my: 2, textAlign: "center" }}>
+              <Button
+                variant="contained"
                 sx={{
-                  bgcolor: theme.tertiaryContainer,
-                  my: 2,
-                  p: 2,
-                  borderRadius: 2,
+                  borderRadius: 12,
+                  background: theme.primaryContainer,
+                  color: theme.onPrimaryContainer,
+                  textTransform: "none",
                 }}
+                href={
+                  "lnreader://repo/add?url=" +
+                  encodeURIComponent(lnreaderPluginsRepo)
+                }
               >
-                <Typography sx={{ mb: 2, fontWeight: "600" }}>
-                  Unsupported operating system
-                </Typography>
-                <Typography sx={{ fontSize: 16 }}>
-                  <b>LNReader</b> is an <b>Android app</b> only. Use an{" "}
-                  <b>Android</b> device with <b>LNReader</b> installed to add
-                  this extension repo.
-                </Typography>
-              </Card>
-            )}
+                Add repository
+              </Button>
+              <Typography sx={{ mt: 2 }}>
+                Requires <b>LNReader 2.0.0</b> or newer.
+              </Typography>
+            </Box>
             <Divider />
           </Box>
         }
